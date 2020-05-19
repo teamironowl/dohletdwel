83b0298a1123a32a7cba52c9fbff9a5d2c4765d1
luffy sai ssll <36154333+sailao@users.noreply.github.com>
2020-05-19 22:36:30 +0630

    first commit

---
+++ app/Http/Controllers/RequestFormController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\RequestForm;
+use Illuminate\Http\Request;
+
+class RequestFormController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\RequestForm  $requestForm
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RequestForm $requestForm)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\RequestForm  $requestForm
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RequestForm $requestForm)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\RequestForm  $requestForm
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RequestForm $requestForm)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\RequestForm  $requestForm
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RequestForm $requestForm)
+    {
+        //
+    }
+}
---
+++ database/factories/RequestFormFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\RequestForm;
+use Faker\Generator as Faker;
+
+$factory->define(RequestForm::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
