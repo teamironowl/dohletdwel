9d0c781b949866908d39f8193123d7aad99818e7
teamironowl <teamironowl@gmail.com>
2020-05-24 22:17:07 +0630

    add map

--- resources/views/map/map.blade.php
+++ resources/views/map/map.blade.php
@@ -1,2 +1,5 @@
 @extends('layouts.app')
 @section('title', 'မြေပုံ')
+@section('content')
+<iframe width="600" height="450" src="https://datastudio.google.com/embed/reporting/16762e14-aad3-4084-8fc5-98f5009eb6da/page/525MB" frameborder="0" style="border:0" allowfullscreen></iframe>
+@endsection
