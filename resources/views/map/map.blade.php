ea2a111b1a154b088e9d978f65499514254fb211
teamironowl <teamironowl@gmail.com>
2020-05-30 16:31:26 +0630

    fix tree map

--- resources/views/map/map.blade.php
+++ resources/views/map/map.blade.php
@@ -2,6 +2,6 @@
 @section('title', 'မြေပုံ')
 @section('content')
 <div class="container-fluid text-center">
-    <iframe class="w-100 vh-100" src="https://datastudio.google.com/embed/u/0/reporting/5ed9d538-9c75-4427-8593-75a27ed1519f/page/6KaRB" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <iframe width="1024" height="720" src="https://datastudio.google.com/embed/u/0/reporting/5ed9d538-9c75-4427-8593-75a27ed1519f/page/6KaRB" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
 @endsection
