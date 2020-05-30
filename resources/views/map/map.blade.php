1461f6fedc340956acf6363041f265fe205f82b5
teamironowl <teamironowl@gmail.com>
2020-05-30 16:22:27 +0630

    google amp

--- resources/views/map/map.blade.php
+++ resources/views/map/map.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 @section('title', 'မြေပုံ')
 @section('content')
-<iframe width="1024" height="450" src="https://datastudio.google.com/embed/reporting/16762e14-aad3-4084-8fc5-98f5009eb6da/page/525MB" frameborder="0" style="border:0" allowfullscreen></iframe>
+<div class="container-fluid text-center">
+    <iframe width="1024" height="720" src="https://datastudio.google.com/embed/u/0/reporting/5ed9d538-9c75-4427-8593-75a27ed1519f/page/6KaRB" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
 @endsection
